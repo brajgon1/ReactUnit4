793f6f34bfdfbb256bf713b0f5d579660dbd25ce
Brandon Gonzalez <brajgon@gmail.com>
2024-06-11 19:44:41 -0600

    added middleware, endpoints to index.js

---
+++ starter/server/controllers/auth.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+login: async (req, res) => {
+    console.log('login')
+    res.sendStatus(200)
+},
+
+register: async (req, res) => {
+    console.log('register')
+    res.sendStatus(200)
+},
+
+};
